662d40c1552e18381148391ffb74f39285f8fc5b
saxumcordis <victor123952@gmail.com>
2024-03-12 10:25:14 +0300

    add initImages script

--- index.js
+++ index.js
@@ -1,32 +1,117 @@
-const openButton = document.querySelector('button');
-const closeButton = document.querySelector('.close');
+const DEFAULT_IMAGES = [
+  {
+    "id": "kt",
+    "url": "https://cdn2.thecatapi.com/images/kt.jpg",
+    "width": 1600,
+    "height": 1067
+  },
+  {
+    "id": "m2",
+    "url": "https://cdn2.thecatapi.com/images/m2.jpg",
+    "width": 500,
+    "height": 500
+  },
+  {
+    "id": "18d",
+    "url": "https://cdn2.thecatapi.com/images/18d.gif",
+    "width": 372,
+    "height": 242
+  },
+  {
+    "id": "6tk",
+    "url": "https://cdn2.thecatapi.com/images/6tk.jpg",
+    "width": 640,
+    "height": 426
+  },
+  {
+    "id": "bdu",
+    "url": "https://cdn2.thecatapi.com/images/bdu.jpg",
+    "width": 500,
+    "height": 328
+  },
+  {
+    "id": "bjh",
+    "url": "https://cdn2.thecatapi.com/images/bjh.jpg",
+    "width": 640,
+    "height": 360
+  },
+  {
+    "id": "cqj",
+    "url": "https://cdn2.thecatapi.com/images/cqj.jpg",
+    "width": 3870,
+    "height": 3104
+  },
+  {
+    "id": "MjAwMzg3Ng",
+    "url": "https://cdn2.thecatapi.com/images/MjAwMzg3Ng.jpg",
+    "width": 1000,
+    "height": 664
+  },
+  {
+    "id": "tZYP8bcem",
+    "url": "https://cdn2.thecatapi.com/images/tZYP8bcem.jpg",
+    "width": 1200,
+    "height": 900
+  },
+  {
+    "id": "jyjEWK7xp",
+    "url": "https://cdn2.thecatapi.com/images/jyjEWK7xp.jpg",
+    "width": 3144,
+    "height": 2448
+  }
+]
 
-/* navigator.geolocation.getCurrentPosition(function (location) {
-  console.log(location.coords.latitude);
-  console.log(location.coords.longitude);
-  console.log(location.coords.accuracy);
-}); */
+const createImgToUl = (url) => {
+  const li = document.createElement('li');
+  const img = document.createElement('img');
 
-openButton.onclick = () => {
-  const modal = document.querySelector('.modal')
+  img.setAttribute('src', url)
+  img.className = 'list__item_image';
 
-  modal.style.display = 'block';
+  li.className = 'list__item'
+
+  return li;
 }
 
-closeButton.onclick = () => {
-  const modal = document.querySelector('.modal')
+function initImages() {
+  const gallery = document.querySelector('.gallery');
+  const ul = document.createElement('ul');
+  ul.className = 'list';
 
-  modal.style.display = 'none';
-}
-
-fetch('https://api.thecatapi.com/v1/images/search?limit=10').then(e => e.json()).then(response => {
-  const modal = document.querySelector('.modal')
-
-  response.forEach((elem) => {
+  DEFAULT_IMAGES.forEach(image => {
+    const li = document.createElement('li');
     const img = document.createElement('img');
-    img.setAttribute('src', elem.url);
-    modal.append(img);
-  })
-})
 
+    img.setAttribute('src', image.url)
+    img.className = 'list__item_image';
+
+    li.className = 'list__item'
+    li.append(img);
+
+    ul.append(li)
+  })
+
+  gallery.append(ul);
+}
+
+function initCreateButton() {
+  const button = document.querySelector('.create-elem');
+  const ul = document.querySelector('.list');
+
+
+  button.onclick = () => {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+
+    img.setAttribute('src', 'https://cdn2.thecatapi.com/images/tZYP8bcem.jpg')
+    img.className = 'list__item_image';
+
+    li.className = 'list__item'
+    li.append(img);
+    ul.append(li)
+  }
+}
+
+initImages();
+initCreateButton();
 
